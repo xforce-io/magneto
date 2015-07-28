c159b87af52ee5924c6808047ad31822f90b9d29
freeman <freeman@zamplus.com>
2015-07-28 16:47:40 +0800

    modify interface

--- src/public_model.h
+++ src/public_model.h
@@ -8,7 +8,7 @@ namespace magneto {
 typedef std::pair<Slice, const void*> Buf;
 typedef std::vector<const Buf*> Bufs;
 typedef std::pair<RoutineHandler, size_t> RoutineItem;
-typedef std::vector< std::pair<RoutineHandler, size_t> > RoutineItems;
+typedef std::vector<RoutineItem> RoutineItems;
 typedef std::pair<int, ProtocolRead*> Response;
 typedef std::vector<Response> Responses;
 typedef std::vector<int> Errors;
